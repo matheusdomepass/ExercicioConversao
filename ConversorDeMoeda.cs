eac80ec5e0be405b94d7a6e65a5410312e05239f
Matheus Passatuto <50487734+matheusdomepass@users.noreply.github.com>
2024-06-18 10:10:12 -0300

    Add files via upload

---
+++ ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioConversao
+{
+    class ConversorDeMoeda
+    {
+        public static double IoF = 6.0;
+       
+        public static double Conversao(double valor, double cotacao)
+        {
+            double total = valor * cotacao;
+            return total + total * IoF / 100.0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioConversao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação em dólar: ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = ConversorDeMoeda.Conversao(valor, cotacao);
+
+            Console.WriteLine("Valor a ser pago em reais = " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
